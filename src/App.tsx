bbe86b7fc53f9b56a13149250b73995f1405acb8
kmsheng <kmsh3ng@gmail.com>
2022-12-08 18:04:18 +0800

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PasswordInput from './components/PasswordInput'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App bg-[#181818] text-[#fff]">
+    <div className="App bg-[#181818] text-[#fff] min-h-screen flex justify-center items-center">
+      <PasswordInput />
     </div>
   );
 }
