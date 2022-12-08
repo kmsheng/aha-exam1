04edfd59fc5881dbe7c3c920deb269c74fdd22b5
kmsheng <kmsh3ng@gmail.com>
2022-12-08 22:53:54 +0800

    add consts/pasword.ts

---
+++ src/consts/password.ts
@@ -0,0 +1,11 @@
+export type RuleItem = {
+  text: string;
+}
+
+export const ruleItems: RuleItem[] = [
+  { text: 'Have at least one uppercase letter' },
+  { text: 'Have at least one lowercase letter' },
+  { text: 'Have at least one number' },
+  { text: 'Have at least one special character (!@#$...etc)' },
+  { text: 'Longer than 8 characters' },
+]
