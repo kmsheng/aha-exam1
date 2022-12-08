655bd94c03b00f79d729447c26db171bec420758
kmsheng <kmsh3ng@gmail.com>
2022-12-08 22:59:48 +0800

    add enum PasswordRules

--- src/consts/password.ts
+++ src/consts/password.ts
@@ -1,11 +1,35 @@
+export enum PasswordRules {
+  UPPERCASE_REQUIRED,
+  LOWERCASE_REQUIRED,
+  NUMNER_REQUIRED,
+  SPECIAL_CHAR_REQUIRED,
+  LONGER_THAN_EIGHT_CHARS
+}
+
 export type RuleItem = {
   text: string;
+  type: PasswordRules
 }
 
 export const ruleItems: RuleItem[] = [
-  { text: 'Have at least one uppercase letter' },
-  { text: 'Have at least one lowercase letter' },
-  { text: 'Have at least one number' },
-  { text: 'Have at least one special character (!@#$...etc)' },
-  { text: 'Longer than 8 characters' },
+  {
+    type: PasswordRules.UPPERCASE_REQUIRED,
+    text: 'Have at least one uppercase letter'
+  },
+  {
+    type: PasswordRules.LOWERCASE_REQUIRED,
+    text: 'Have at least one lowercase letter'
+  },
+  {
+    type: PasswordRules.NUMNER_REQUIRED,
+    text: 'Have at least one number'
+  },
+  {
+    type: PasswordRules.SPECIAL_CHAR_REQUIRED,
+    text: 'Have at least one special character (!@#$...etc)'
+  },
+  {
+    type: PasswordRules.LONGER_THAN_EIGHT_CHARS,
+    text: 'Longer than 8 characters'
+  },
 ]
